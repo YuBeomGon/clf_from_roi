e4bad2d5e55470e8f655f7d683a9823702b04b8c
YuBeomGon <cleo8131@naver.com>
2022-05-10 11:08:12 +0900

    fix for out of shared memory

--- run.sh
+++ run.sh
@@ -2,8 +2,11 @@
 # arch='swin_t'
 arch='resnet18'
 
-docker run --gpus all -it --rm -v $PWD:/opt -v /home/Dataset/scl/patch_images:/opt/lbp_data/patch_images $@ beomgon/pl_deepspeed:paps_contra --arch $arch
+# https://docs.docker.com/engine/reference/run/#ipc-settings---ipc
+# set --ipc=host for solving out of shared memory 
 
-docker run --gpus all -it --rm -v $PWD:/opt -v /home/Dataset/scl/patch_images:/opt/lbp_data/patch_images $@ beomgon/pl_deepspeed:paps_downstream  --arch $arch
+docker run --gpus all -it --ipc=host --rm -v $PWD:/opt -v /home/Dataset/scl/patch_images:/opt/lbp_data/patch_images $@ beomgon/pl_deepspeed:paps_contra --arch $arch
 
-docker run --gpus all -it --rm -v $PWD:/opt -v /home/Dataset/scl/patch_images:/opt/lbp_data/patch_images $@ beomgon/pl_deepspeed:paps_test  --arch $arch
+docker run --gpus all -it --ipc=host --rm -v $PWD:/opt -v /home/Dataset/scl/patch_images:/opt/lbp_data/patch_images $@ beomgon/pl_deepspeed:paps_downstream  --arch $arch
+
+docker run --gpus all -it --ipc=host --rm -v $PWD:/opt -v /home/Dataset/scl/patch_images:/opt/lbp_data/patch_images $@ beomgon/pl_deepspeed:paps_test  --arch $arch
