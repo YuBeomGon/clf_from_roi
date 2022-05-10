323f0dae5a406566f16a4073cbf99e538fe7ddda
YuBeomGon <cleo8131@naver.com>
2022-05-10 09:37:16 +0900

    make docker file for test

--- docker_push.sh
+++ docker_push.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # build the base image
-docker build --file dockerfiles/dockerfile.pl.deepspeed -t beomgon/pl_deepspeed:base .
+docker build --file dockerfiles/dockerfile.pl.deepspeed -t beomgon/pl_deepspeed:paps_base .
 
-docker build --file dockerfiles/dockerfile.downstream -t beomgon/pl_deepspeed:downstream .
+docker build --file dockerfiles/dockerfile.downstream -t beomgon/pl_deepspeed:paps_downstream .
 
-# docker build --file dockerfiles/dockerfile.sup_contra -t beomgon/pl_deepspeed:supervised_contrastive .
+docker build --file dockerfiles/dockerfile.sup_contra -t beomgon/pl_deepspeed:paps_contra .
+
+docker build --file dockerfiles/dockerfile.test -t beomgon/pl_deepspeed:paps_test .
 
 # push the image to dockhub(registry server)
-docker push beomgon/pl_deepspeed:base
+docker push beomgon/pl_deepspeed:paps_base
 
-docker push beomgon/pl_deepspeed:downstream
+docker push beomgon/pl_deepspeed:paps_downstream
+
+docker push beomgon/pl_deepspeed:paps_contra
+
+docker push beomgon/pl_deepspeed:paps_test
 
 #docker push beomgon/pl_deepspeed:supervised_contrastive
 
