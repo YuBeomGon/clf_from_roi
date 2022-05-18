3fc5192766804b8e5c6f87f05298623ee4db87ec
YuBeomGon <cleo8131@naver.com>
2022-05-18 18:12:52 +0900

    add 19 carcinoma wsi

--- prepare_data/unzip.py
+++ prepare_data/unzip.py
@@ -1,24 +1,25 @@
 import os, zipfile
 
-folder = '/home/beomgon/pytorch/LBP_scl/PapSwinObjDet/data/'
-anno = 'annotations/'
+cur_dir = '/'.join(os.path.abspath(__file__).split('/')[:-1])
+anno_dir = 'annotations/'
+
 extension = ".zip"
 cnt1 = 0
 cnt2 = 0
-for file in os.listdir(folder+anno):
+for file in os.listdir(cur_dir+ '/' + anno_dir) :
     print(file)
     if file.endswith('.zip') :
         if not file.startswith('drive-') :
             cnt1 += 1
             d = file.split('-c')[0]
 #             print(d)
-            zip = zipfile.ZipFile(anno + file)
-            if not os.path.isdir(anno + d) :
+            zip = zipfile.ZipFile(anno_dir + file)
+            if not os.path.isdir(anno_dir + d) :
                 cnt2 += 1
                 print(d)
-                os.mkdir(anno + d)        
-                zip.extractall(path=anno + d)
+                os.mkdir(anno_dir + d)        
+                zip.extractall(path=anno_dir + d)
         #         zipfile.ZipFile.extract(item)
             #     if item.endswith(extension):
             #         zipfile.ZipFile.extract(item)
-print('cnt1 {} cnt2 {}'.format(cnt1, cnt2))            
+print('total folder {} newly created folder {}'.format(cnt1, cnt2))            
